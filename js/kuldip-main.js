bb2b253369906abf95a3696832e6b3d153bc720d
kuldip <n01415804@humbermail.ca>
2020-12-07 11:06:31 +0530

    Project_Phase_2

--- js/kuldip-main.js
+++ js/kuldip-main.js
@@ -54,64 +54,69 @@ function toogle_visibility(tab_id, id) {
     Order Timeline
 -------------------------------------------------------------------*/
 
-orderdiv = document.createElement("div");
-orderdiv.classList.add("medium-12", "description");
+function load_phase() {
 
-orderh3 = document.createElement("h3");
-orderh3.textContent = "Ordered Phase:- ";
+    loadphasebtn = document.querySelector('#loadphasebtn');
+    loadphasebtn.style.display = 'none';
 
-orderp = document.createElement("p");
-orderp.textContent = "During this phase we record all your order details and confirm that payment has been received. After getting Confirmation ordered food will be ready by cooks and then perform quality check.In the last order will be packed and ready for shipped.";
+    orderdiv = document.createElement("div");
+    orderdiv.classList.add("medium-12", "description");
 
-orderdiv.append(orderh3);
-orderdiv.append(orderp);
+    orderh3 = document.createElement("h3");
+    orderh3.textContent = "Ordered Phase:- ";
 
-descdiv = document.querySelector('.progress .row');
-descdiv.append(orderdiv);
+    orderp = document.createElement("p");
+    orderp.textContent = "During this phase we record all your order details and confirm that payment has been received. After getting Confirmation ordered food will be ready by cooks and then perform quality check.In the last order will be packed and ready for shipped.";
 
-shipdiv = document.createElement("div");
-shipdiv.classList.add("medium-12", "description");
+    orderdiv.append(orderh3);
+    orderdiv.append(orderp);
 
-shiph3 = document.createElement("h3");
-shiph3.textContent = "Shipped Phase:- ";
+    descdiv = document.querySelector('#loadphase');
+    descdiv.append(orderdiv);
 
-shipp = document.createElement("p");
-shipp.textContent = "In this phase, first of all we print the order invoice and packing slip. After that we pack the order securely and attach the shipping label to the package. In the end parcel will be ready for dispatch. Moreover, E-mail is sent to the customer about order status and delivery date so they can follow proper timeline.";
+    shipdiv = document.createElement("div");
+    shipdiv.classList.add("medium-12", "description");
 
-shipdiv.append(shiph3);
-shipdiv.append(shipp);
+    shiph3 = document.createElement("h3");
+    shiph3.textContent = "Shipped Phase:- ";
 
-descdiv.append(shipdiv);
+    shipp = document.createElement("p");
+    shipp.textContent = "In this phase, first of all we print the order invoice and packing slip. After that we pack the order securely and attach the shipping label to the package. In the end parcel will be ready for dispatch. Moreover, E-mail is sent to the customer about order status and delivery date so they can follow proper timeline.";
 
-dispdiv = document.createElement("div");
-dispdiv.classList.add("medium-12", "description");
+    shipdiv.append(shiph3);
+    shipdiv.append(shipp);
 
-disph3 = document.createElement("h3");
-disph3.textContent = "Dispatched Phase:- ";
+    descdiv.append(shipdiv);
 
-dispp = document.createElement("p");
-dispp.textContent = "This is the phase where order has been picked from restaurant and get out for delivery. In the first step of this phase we examine your other orders for delivery. Then we load item into the delivery vehicle and order go through diffent delivery center to main customer location.";
+    dispdiv = document.createElement("div");
+    dispdiv.classList.add("medium-12", "description");
 
-dispdiv.append(disph3);
-dispdiv.append(dispp);
+    disph3 = document.createElement("h3");
+    disph3.textContent = "Dispatched Phase:- ";
 
-descdiv.append(dispdiv);
+    dispp = document.createElement("p");
+    dispp.textContent = "This is the phase where order has been picked from restaurant and get out for delivery. In the first step of this phase we examine your other orders for delivery. Then we load item into the delivery vehicle and order go through diffent delivery center to main customer location.";
 
-delidiv = document.createElement("div");
-delidiv.classList.add("medium-12", "description");
+    dispdiv.append(disph3);
+    dispdiv.append(dispp);
 
-delih3 = document.createElement("h3");
-delih3.textContent = "Deliverd Phase:- ";
+    descdiv.append(dispdiv);
 
-delip = document.createElement("p");
-delip.textContent = "This is the final phase where order has been deliverd to Customer location. After that E-mail and message has been sent to the customer for confirmation.";
+    delidiv = document.createElement("div");
+    delidiv.classList.add("medium-12", "description");
 
-delidiv.append(delih3);
-delidiv.append(delip);
+    delih3 = document.createElement("h3");
+    delih3.textContent = "Deliverd Phase:- ";
 
-descdiv.append(delidiv);
+    delip = document.createElement("p");
+    delip.textContent = "This is the final phase where order has been deliverd to Customer location. After that E-mail and message has been sent to the customer for confirmation.";
 
+    delidiv.append(delih3);
+    delidiv.append(delip);
 
+    descdiv.append(delidiv);
+
+}
 
 /*------------------------------------------------------------------
     Customer Reviews
